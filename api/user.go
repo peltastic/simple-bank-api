4b90b863b4d7a01a36fdc60e9fa03080a8393a4f
square <onosogapelumi@gmail.com>
2022-03-24 11:42:56 +0100

    authentication

--- api/user.go
+++ api/user.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 	"time"
 
@@ -25,15 +26,15 @@ type userResponse struct {
 	CreatedAt         time.Time `json:"created_at"`
 }
 
-// func newUserResponse(user db.User) userResponse {
-// 	return userResponse{
-// 		Username:          user.Username,
-// 		FullName:          user.FullName,
-// 		Email:             user.Email,
-// 		PasswordChangedAt: user.PasswordChangedAt,
-// 		CreatedAt:         user.CreatedAt,
-// 	}
-// }
+func newUserResponse(user db.User) userResponse {
+	return userResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	}
+}
 
 func (server *Server) createUser(ctx *gin.Context) {
 	var req createUserRequest
@@ -78,74 +79,56 @@ func (server *Server) createUser(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, rsp)
 }
 
-// type loginUserRequest struct {
-// 	Username string `json:"username" binding:"required,alphanum"`
-// 	Password string `json:"password" binding:"required,min=6"`
-// }
+type loginUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+}
 
-// type loginUserResponse struct {
-// 	SessionID             uuid.UUID    `json:"session_id"`
-// 	AccessToken           string       `json:"access_token"`
-// 	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
-// 	RefreshToken          string       `json:"refresh_token"`
-// 	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
-// 	User                  userResponse `json:"user"`
-// }
+type loginUserResponse struct {
+	AccessToken string       `json:"access_token"`
+	User        userResponse `json:"user"`
+}
 
-// func (server *Server) loginUser(ctx *gin.Context) {
-// 	var req loginUserRequest
-// 	if err := ctx.ShouldBindJSON(&req); err != nil {
-// 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-// 		return
-// 	}
+func (server *Server) loginUser(ctx *gin.Context) {
+	var req loginUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
 
-// 	user, err := server.store.GetUser(ctx, req.Username)
-// 	if err != nil {
-// 		if err == sql.ErrNoRows {
-// 			ctx.JSON(http.StatusNotFound, errorResponse(err))
-// 			return
-// 		}
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-// 	err = util.CheckPassword(req.Password, user.HashedPassword)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
-// 		return
-// 	}
+	err = util.CheckPassword(req.Password, user.HashedPassword)
+	if err != nil {
+		ctx.JSON(http.StatusUnauthorized, errorResponse(err))
+		return
+	}
 
-// 	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
-// 		user.Username,
-// 		server.config.AccessTokenDuration,
-// 	)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	accessToken, _,err := server.tokenMaker.CreateToken(
+		user.Username,
+		server.config.AccessTokenDuration,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-// 	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
-// 		user.Username,
-// 		server.config.RefreshTokenDuration,
-// 	)
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+	rsp := loginUserResponse{
 
-// 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
-// 		ID:           refreshPayload.ID,
-// 		Username:     user.Username,
-// 		RefreshToken: refreshToken,
-// 		UserAgent:    ctx.Request.UserAgent(),
-// 		ClientIp:     ctx.ClientIP(),
-// 		IsBlocked:    false,
-// 		ExpiresAt:    refreshPayload.ExpiredAt,
-// 	})
-// 	if err != nil {
-// 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-// 		return
-// 	}
+		AccessToken: accessToken,
+		User:        newUserResponse(user),
+	}
+	ctx.JSON(http.StatusOK, rsp)
+
+}
 
 // 	rsp := loginUserResponse{
 // 		SessionID:             session.ID,
--- go.mod
+++ go.mod
@@ -4,9 +4,17 @@ go 1.17
 
 require github.com/lib/pq v1.10.4
 
-require github.com/spf13/viper v1.10.1
+require (
+	github.com/aead/chacha20poly1305 v0.0.0-20201124145622-1a5aba2a8b29
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/google/uuid v1.3.0
+	github.com/o1egl/paseto v1.0.0
+	github.com/spf13/viper v1.10.1
+)
 
 require (
+	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
+	github.com/aead/poly1305 v0.0.0-20180717145839-3fee0db0b635 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -22,6 +30,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.8.1 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
@@ -42,6 +51,6 @@ require (
 	github.com/golang/mock v1.6.0
 	github.com/stretchr/testify v1.7.0
 	github.com/techschool/simplebank v0.0.0-20220312094725-6d1d65f9dd52
-	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70 // indirect
+	golang.org/x/crypto v0.0.0-20220307211146-efcb8507fb70
 	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 )
